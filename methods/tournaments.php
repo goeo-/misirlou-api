ef140da7f4faef2f3f1d22be5abd36dada728103
Morgan Bazalgette <the@howl.moe>
2017-08-01 21:35:10 +0200

    Fix my_team giving random values

--- methods/tournaments.php
+++ methods/tournaments.php
@@ -16,17 +16,14 @@ function run_method($state)
 	t.id, t.name, t.description, t.mode, t.status,
 	t.status_data, t.created_at, t.updated_at,
 	t.team_size, t.min_team_size, t.exclusivity_starts,
-	t.exclusivity_ends,	MAX(tu.team) AS my_team, teams.name as my_team_name,
-	t.max_beatmap_requests
+	t.exclusivity_ends,	t.max_beatmap_requests
 FROM tournaments t
-LEFT JOIN teams ON teams.tournament = t.id
-LEFT JOIN team_users tu ON tu.team = teams.id AND tu.user = ? AND tu.attributes != 0
 ";
 	$query .= build_where($parts);
 	$query .= " GROUP BY t.id ORDER BY t.updated_at DESC LIMIT $offset, 50";
 
 	$results = $state->db->fetchAll($query, $params);
-	array_walk($results, 'walker');
+	array_walk($results, 'walker', $state);
 
 	echo json_encode([
 		"ok" => true,
@@ -34,7 +31,7 @@ LEFT JOIN team_users tu ON tu.team = teams.id AND tu.user = ? AND tu.attributes 
 	], JSON_HEX_TAG);
 }
 
-function walker(&$el)
+function walker(&$el, $key, $state)
 {
 	$el["id"]                   = (int) $el["id"];
 	$el["mode"]                 = (int) $el["mode"];
@@ -53,7 +50,14 @@ function walker(&$el)
 	$el["created_at"] = to_3339($el["created_at"]);
 	$el["updated_at"] = to_3339($el["updated_at"]);
 
-	if ($el["my_team"] !== null) {
-		$el["my_team"] = (int) $el["my_team"];
+	$myTeam = $state->db->fetch("SELECT tu.team AS my_team, teams.name as my_team_name
+FROM team_users tu
+INNER JOIN teams ON teams.id = tu.team
+WHERE teams.tournament = ? AND tu.attributes > 0
+LIMIT 1", [$el["id"]]);
+
+	if ($myTeam) {
+		$el["my_team"] = (int) $myTeam["my_team"];
+		$el["my_team_name"] = $myTeam["my_team_name"];
 	}
 }
